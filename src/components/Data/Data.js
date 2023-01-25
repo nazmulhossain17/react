77a1fad03e8d89e14c2a9edf97a0a8f86a5e478d
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-01-25 13:05:46 +0600

    local-storage

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Classes from '../Classes/Classes'
 import './Data.css';
@@ -7,15 +8,36 @@ const Data = () => {
     const [info, setInfo] = useState([]);
     const [cart, setCart] = useState([]);
     useEffect(()=>{
+      console.log('products load before fetch')
         fetch('data.json')
         .then(res => res.json())
-        .then(data => setInfo(data))
-    },[])
+        .then(data => {
+          setInfo(data)
+          // console.log('product loaded')
+          })
+      },[])
+
+    useEffect(()=>{
+      console.log('local storage 1st line', info)
+      const storedCart = getStoredCart();
+      const savedCart = [];
+      for(const id in storedCart){
+        const addedInfo = info.find(n => n.id === id);
+        if(addedInfo){
+          const quantity = storedCart[id]
+          addedInfo.quantity = quantity
+          savedCart.push(addedInfo)
+        }
+      }
+      setCart(savedCart)
+      // console.log('local storage finished')
+    },[info])
 
     const handleClick = (info) =>{
-      console.log(info);
+      // console.log(info);
       const newCart = [...cart, info];
       setCart(newCart);
+      addToDb(info.id);
   }
     return (
         <div className='shop-container'>
